9bab81c9578294f95d5fbd8ba239596b638c082e
Howard <howard@vinova.sg>
2018-12-11 14:00:44 +0700

    + add Readme for week 1

--- week_1/App.js
+++ week_1/App.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TabBarIOS} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,12 +14,35 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      <TabBarIOS
+        unselectedTintColor="#CCC"
+        tintColor="yellow"
+        unselectedItemTintColor="#CCC"
+        barTintColor="darkslateblue">
+        <TabBarIOS.Item
+          title="Home"
+//          selected={this.state.selectedTab === 'homeTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'homeTab',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Home tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="featured"
+          badgeColor="pink"
+          selected={this.state.selectedTab === 'newMobieTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'newMobieTab',              
+            });
+          }}>
+          {this._renderContent('#783E33', 'Featured Tab')}
+        </TabBarIOS.Item>
+   
+      </TabBarIOS>
+    );  
   }
 }
 
